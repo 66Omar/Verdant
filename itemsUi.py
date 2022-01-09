389cece1fcee869a983bfd41cb0d2d5c24677b7e
Omar <omar.saidmady@gmail.com>
2022-01-09 23:21:32 +0200

    :framed_picture: Added item image and video format indicator

--- itemsUi.py
+++ itemsUi.py
@@ -1,6 +1,9 @@
+import requests
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QVBoxLayout, QLabel, QHBoxLayout, QListWidget, QListWidgetItem, QPushButton
+from PyQt5.QtGui import QPixmap
 from quality_dialog import QualityDialog
+from PyQt5.QtCore import Qt
 
 
 class QCustomQWidget(QtWidgets.QWidget):
@@ -9,18 +12,36 @@ class QCustomQWidget(QtWidgets.QWidget):
         self.textQVBoxLayout = QVBoxLayout()
         self.textUpQLabel = QLabel()
         self.textDownQLabel = QLabel()
+        self.qualityQLabel = QLabel()
         self.download = QPushButton()
         self.download.setFixedWidth(200)
         self.download.setFixedHeight(30)
         self.download.setText("Download")
         self.download.hide()
+
+        self.image_label = QLabel()
+        self.image = QPixmap()
+
+        self.image_label.setStyleSheet('''
+            QLabel {
+            margin-left: 30px;
+            }
+        ''')
+        self.textQVBoxLayout.setSpacing(0)
+        self.textQVBoxLayout.addStretch(1)
         self.textQVBoxLayout.addWidget(self.textUpQLabel)
         self.textQVBoxLayout.addWidget(self.textDownQLabel)
+        self.textQVBoxLayout.addWidget(self.qualityQLabel)
+        self.textQVBoxLayout.setSpacing(0)
+        self.textQVBoxLayout.addStretch(1)
 
         self.allQHBoxLayout = QHBoxLayout()
         self.iconQLabel = QLabel()
-        self.allQHBoxLayout.addLayout(self.textQVBoxLayout, 0)
-        self.allQHBoxLayout.addWidget(self.download, 1)
+        self.allQHBoxLayout.setSpacing(0)
+        self.allQHBoxLayout.addWidget(self.image_label, 0)
+        self.allQHBoxLayout.addLayout(self.textQVBoxLayout, 1)
+        self.allQHBoxLayout.addWidget(self.download, 2)
+
 
         self.setLayout(self.allQHBoxLayout)
         # setStyleSheet
@@ -28,16 +49,28 @@ class QCustomQWidget(QtWidgets.QWidget):
             QLabel {
             color: white;
             margin-left: 100px;
+            font-weight: bold;
             font-size: 13px;
             }
         ''')
         self.textDownQLabel.setStyleSheet('''
             QLabel {
-            color: white;
+            color: coral;
+            margin-top: 70px;
             margin-left: 100px;
-            font-size: 13px;
+            font-size: 12px;
             }
         ''')
+        self.qualityQLabel.setStyleSheet('''
+                    QLabel {
+                    color: #81D8D0;
+                    margin-top: 30px;
+                    margin-left: 100px;
+                    font-size: 12px;
+                    }
+                ''')
+
+
         self.download.setStyleSheet('''
             QPushButton {
             margin-right: 100px;
@@ -65,6 +98,9 @@ class QCustomQWidget(QtWidgets.QWidget):
     def setTextDown(self, text):
         self.textDownQLabel.setText(text)
 
+    def set_qualit_text(self, text):
+        self.qualityQLabel.setText(text)
+
     def showDownload(self):
         self.download.show()
 
@@ -73,6 +109,17 @@ class QCustomQWidget(QtWidgets.QWidget):
 
     def get_text_down(self):
         return self.textDownQLabel.text()
+
+    def set_image(self, url):
+        try:
+            data = requests.get(url).content
+            self.image.loadFromData(data)
+            self.image = self.image.scaled(250, 250, Qt.KeepAspectRatio)
+            self.image_label.setFixedWidth(250)
+            self.image_label.setFixedHeight(250)
+            self.image_label.setPixmap(self.image)
+        except Exception as e:
+            print(e)
 
 
 class Items(QListWidget):
@@ -120,7 +167,7 @@ class Items(QListWidget):
         for name in kwagrs:
             show = False
             series = kwagrs[name]
-            if 'series' in series:
+            if 'series' in series[0]:
                 series = 'Series'
             else:
                 series = 'Movie'
@@ -129,6 +176,8 @@ class Items(QListWidget):
             self.myQCustomQWidget = QCustomQWidget()
             self.myQCustomQWidget.setTextUp(name)
             self.myQCustomQWidget.setTextDown(series)
+            self.myQCustomQWidget.set_image(kwagrs[name][1])
+            self.myQCustomQWidget.set_qualit_text(kwagrs[name][2])
             if show:
                 self.myQCustomQWidget.showDownload()
                 self.myQCustomQWidget.download.clicked.connect(lambda: self.go_to_download())
@@ -144,11 +193,12 @@ class Items(QListWidget):
             dialog.exec_()
             if dialog.quality is not None:
                 name = self.sender().parent().get_text()
-                items = [self.needed[name]]
+                items = [self.needed[name][0]]
                 self.parent().parent().name = name
                 self.parent().parent().prepare_download_page(0, items, dialog.quality)
 
     def check_and_initialize(self, item):
         current_item = self.itemWidget(item).get_text_down()
+        name = self.itemWidget(item).get_text()
         if str(current_item).lower() == 'series':
-            self.parent().parent().init_seasons_worker(item)
+            self.parent().parent().init_seasons_worker(item, name)
