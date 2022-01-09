b1c54f38b59bee832108644185e971cf801697fc
Omar <omar.saidmady@gmail.com>
2022-01-09 23:58:48 +0200

    :robot: adjusted selenium settings, removed duplicated calls, added progress signal

--- Workers/download_worker.py
+++ Workers/download_worker.py
@@ -3,9 +3,13 @@ from Scraping.prepare_download import get_download_link
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+import os
+
+os.environ['WDM_LOG_LEVEL'] = '0'
 
 
 class DownloadWorker(QThread):
+    state = pyqtSignal(int)
     finished = pyqtSignal()
     update = pyqtSignal(list)
     send = pyqtSignal(str)
@@ -25,6 +29,7 @@ class DownloadWorker(QThread):
                 break
             self.driver = None
             self.update.emit([each, 0])
+            self.state.emit(0)
             link = self.item_parent.itemWidget(each).get_text_down()
             load_time = 4.5
             got_link = False
@@ -40,22 +45,26 @@ class DownloadWorker(QThread):
                     self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options,
                                                    service_args=["hide_console", ])
 
-                    x = get_download_link(self.driver, link, load_time, str(self.quality))
+                    x = get_download_link(self, self.driver, link, load_time, str(self.quality))
                     if '.mp4' in x[1] or '[EgyBest]' in x[1] or self.quality in x[1]:
+                        self.state.emit(1)
                         got_link = True
-                        self.update.emit([each, 1])
-                        if self.item_parent.parent().parent().auto:
-                            self.send.emit(str(x[0]))
-                        else:
+                        self.driver.quit()
+                        if self.con:
+                            self.state.emit(2)
+                            self.update.emit([each, 1])
                             self.item_parent.itemWidget(each).show_btn()
                             self.item_parent.itemWidget(each).set_link(str(x[0]))
+                            if self.item_parent.parent().parent().auto:
+                                self.send.emit(str(x[0]))
+
                         print('finished')
-                        self.driver.quit()
 
                 except Exception as e:
-                    load_time += 0.33333333333
-                    print(str(e.args) + "#123456")
+                    if self.con:
+                        load_time += 0.33333333333
+                        print(str(e.args) + "#123456")
                     if self.driver is not None:
                         self.driver.quit()
-        self.driver.quit()
+        self.driver = None
         self.finished.emit()
